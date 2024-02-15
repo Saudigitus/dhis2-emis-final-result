f92a1824cbd8be77fa1651384d38358768fa5c4b
Clayd Lena <93912102+ClaydLena@users.noreply.github.com>
2024-02-15 10:58:37 +0200

    Update TableComponent.tsx

--- src/components/table/components/table/TableComponent.tsx
+++ src/components/table/components/table/TableComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames';
-import defaultClasses from '../Table.module.css';
+import defaultClasses from '../table.module.css';
 import { TableComponentProps } from '../../../../types/table/components/table/TableComponentTypes';
 
 function TableComponent(props: TableComponentProps): React.ReactElement {
