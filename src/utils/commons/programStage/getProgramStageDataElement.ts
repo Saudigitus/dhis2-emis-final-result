0d4e1bb174ae0a9e3c595ec23f11b335fcf079d6
cnandza <clayd.nandza@saudigitus.org>
2024-02-16 16:30:14 +0200

    EMISWEB-106 Improvement: Adding dynamic header itens

---
+++ src/utils/commons/programStage/getProgramStageDataElement.ts
@@ -0,0 +1,13 @@
+import { useRecoilValue } from "recoil";
+import { ProgramConfigState } from "../../../schema/programSchema";
+import { ProgramConfig } from "../../../types/programConfig/ProgramConfig";
+
+export const getProgramStageDataElement = () => {
+    const programConfigData = useRecoilValue(ProgramConfigState);
+    const programStageDataElement : any = programConfigData.programStages.flat().map((x : any) => {
+        console.log(x)
+        return " "
+    })
+
+    return { programStageDataElement }
+}
--- src/utils/constants/headBar/headBarData.ts
+++ src/utils/constants/headBar/headBarData.ts
@@ -1,7 +1,9 @@
+import { headBarDataElements } from "./headBarDataElements"
 import { type dataStoreRecord } from "../../../types/dataStore/DataStoreConfig"
 import { type SelectedOptionsTypes, type HeadBarTypes } from "../../../types/headBar/HeadBarTypes"
+import { programStageDataElements } from "../../../types/programStageConfig/ProgramStageConfig"
 
-function headBarData(selectedOptions: SelectedOptionsTypes, getDataStoreData: dataStoreRecord): HeadBarTypes[] {
+function headBarData(selectedOptions: SelectedOptionsTypes, getDataStoreData: dataStoreRecord, programStageDataElements: programStageDataElements[]): HeadBarTypes[] {
     return [
         {
             id: "c540ac7c",
@@ -11,24 +13,7 @@ function headBarData(selectedOptions: SelectedOptionsTypes, getDataStoreData: da
             component: "orgUnitTree",
             selected: selectedOptions?.schoolName ? true : false,
         },
-        {
-            id: "981ed8a3",
-            label: "Grade",
-            value: selectedOptions?.grade ?? "Select a grade",
-            placeholder: "Search for grade",
-            dataElementId: getDataStoreData?.registration?.grade,
-            component: "menuItemContainer",
-            selected: selectedOptions?.grade ? true : false,
-        },
-        {
-            id: "7ce5c7f3",
-            label: "Class",
-            value: selectedOptions?.class ?? "Select a class",
-            placeholder: "Search for class",
-            dataElementId: getDataStoreData?.registration?.section,
-            component: "menuItemContainer",
-            selected: selectedOptions?.class ? true : false,
-        },
+        ...headBarDataElements(selectedOptions, getDataStoreData, programStageDataElements),
         {
             id: "j2e9b216",
             label: "Academic Year",
