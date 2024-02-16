1d7bf8bc15b1b7a10cc61bad9ff896a55dccab83
Clayd Lena <93912102+ClaydLena@users.noreply.github.com>
2024-02-16 11:46:16 +0200

    Update SideBarSubItem.tsx

--- src/components/layout/sidebar/components/SideBarSubItem.tsx
+++ src/components/layout/sidebar/components/SideBarSubItem.tsx
@@ -3,26 +3,10 @@ import classNames from 'classnames';
 import Badge from '../../../badge/Badge';
 import style from "../sideBar.module.css"
 import { useConfig } from '@dhis2/app-runtime';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { type SideBarSubItemProps } from '../../../../types/sideBar/SideBarTypes';
-import { useQueryParams } from '../../../../hooks';
 
 export default function SideBarSubItem({ icon, label, showBadge, disabled, route, appName, pathName }: SideBarSubItemProps) {
     const { baseUrl } = useConfig()
-    const location = useLocation()
-    const navigate = useNavigate()
-    const { remove, urlParamiters } = useQueryParams()
-    let locationSearch = location.search
-    
-
-    console.log(location, route)
-
-    useEffect(() => {
-        const urlParams = location.search.split("&")
-        urlParams.map((param : string) => {
-            remove(param.split("=")[0])
-        })
-    },[urlParamiters().sectionType])
 
     return (
         <a href={`${baseUrl}/api/apps/${appName}/index.html#/${route}`} className={style.subItemLink}>
