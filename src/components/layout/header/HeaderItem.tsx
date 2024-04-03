cbb2ac9cd2694a97859227a11fa664f508e1e667
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-03 09:22:47 +0200

    Disable class and grade selections if there's no school selected

--- src/components/layout/header/HeaderItem.tsx
+++ src/components/layout/header/HeaderItem.tsx
@@ -1,26 +1,34 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import classNames from 'classnames'
-import { useRecoilState } from 'recoil'
+import {useRecoilState} from 'recoil'
 import style from "./mainHeader.module.css"
-import { SimpleSearch } from '../../search'
-import { DropdownButton, FlyoutMenu } from "@dhis2/ui"
+import {SimpleSearch} from '../../search'
+import {DropdownButton, FlyoutMenu} from "@dhis2/ui"
 import HeaderResetItemValue from './HeaderResetItemValue'
 import info from "../../../assets/images/headbar/info.svg"
-import { type HeadBarTypes } from '../../../types/headBar/HeadBarTypes'
-import { OuQueryString } from '../../../schema/headerSearchInputSchema'
-import { componentMapping } from '../../../utils/commons/componentMapping'
-import { useDataElementsParamMapping, useQueryParams } from '../../../hooks'
-import { getSelectedKey } from '../../../utils/commons/dataStore/getSelectedKey'
+import {type HeadBarTypes} from '../../../types/headBar/HeadBarTypes'
+import {OuQueryString} from '../../../schema/headerSearchInputSchema'
+import {componentMapping} from '../../../utils/commons/componentMapping'
+import {useDataElementsParamMapping, useQueryParams} from '../../../hooks'
+import {getSelectedKey} from '../../../utils/commons/dataStore/getSelectedKey'
 
 export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
-    const { label, value, placeholder, component, dataElementId, id, selected } = props;
-    const { remove } = useQueryParams()
+    const {
+        label,
+        value,
+        placeholder,
+        component,
+        dataElementId,
+        id,
+        selected,
+        disabled
+    } = props;
+    const {remove} = useQueryParams()
     const [openDropDown, setOpenDropDown] = useState<boolean>(false);
     const Component = (component != null) ? componentMapping[component] : null;
     const paramsMapping = useDataElementsParamMapping()
     const [, setStringQuery] = useRecoilState(OuQueryString);
-    const { getDataStoreData } = getSelectedKey()
-
+    const {getDataStoreData} = getSelectedKey()
 
     const onToggle = () => {
         setStringQuery(undefined)
@@ -28,31 +36,32 @@ export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
     }
 
     const onReset = () => {
-        if (dataElementId)
+        if (dataElementId) {
             remove(paramsMapping[dataElementId as unknown as keyof typeof paramsMapping])
-        else
-            if (id === "c540ac7c") {
-                remove("school");
-                remove("schoolName");
-            }
+        } else if (id === "c540ac7c") {
+            remove("school");
+            remove("schoolName");
+        }
     }
 
     return (
         <DropdownButton
+            disabled={disabled}
             open={openDropDown}
             onClick={onToggle}
             className={classNames(style.HeaderItemContainer, style[id])}
             component={
-                < FlyoutMenu >
+                < FlyoutMenu>
                     <SimpleSearch id={id} placeholder={placeholder}>
-                        {(Component != null) && <Component dataElementId={dataElementId} onToggle={onToggle} />}
+                        {(Component != null) && <Component dataElementId={dataElementId} onToggle={onToggle}/>}
                     </SimpleSearch>
-                </FlyoutMenu >
+                </FlyoutMenu>
             }
         >
             <h5>{label} <span>{value}</span></h5>
-            {(selected && dataElementId !== getDataStoreData?.registration?.academicYear) ? <HeaderResetItemValue onReset={onReset} /> : null}
-            <img src={info} />
-        </DropdownButton >
+            {((selected === true) && dataElementId !== getDataStoreData?.registration?.academicYear) ?
+                <HeaderResetItemValue onReset={onReset}/> : null}
+            <img src={info}/>
+        </DropdownButton>
     )
 }
--- src/components/layout/header/MainHeader.tsx
+++ src/components/layout/header/MainHeader.tsx
@@ -5,9 +5,9 @@ import style from "./mainHeader.module.css"
 import { useQueryParams } from '../../../hooks'
 import { ProgramConfigState } from '../../../schema/programSchema'
 import { headBarData } from '../../../utils/constants/headBar/headBarData'
-import { ProgramConfig } from '../../../types/programConfig/ProgramConfig'
+import { type ProgramConfig } from '../../../types/programConfig/ProgramConfig'
 import { getSelectedKey } from '../../../utils/commons/dataStore/getSelectedKey'
-import { programStageDataElements } from '../../../types/programStageConfig/ProgramStageConfig'
+import { type programStageDataElements } from '../../../types/programStageConfig/ProgramStageConfig'
 import { initializeRulesEngine } from '../../../hooks/programRules/rules-engine/InitializeRulesEngine'
 
 export default function MainHeader(): React.ReactElement {
@@ -25,7 +25,7 @@ export default function MainHeader(): React.ReactElement {
     return (
         <nav className={style.MainHeaderContainer}>
             {headBarData({ selectedOptions, getDataStoreData, programStageDataElements }).map(headerItem => (
-                <HeaderItem key={headerItem.id} id={headerItem.id} dataElementId={headerItem.dataElementId} component={headerItem.component} placeholder={headerItem.placeholder} label={headerItem.label} value={headerItem.value} selected={headerItem.selected} />
+                <HeaderItem disabled={headerItem.disabled} key={headerItem.id} id={headerItem.id} dataElementId={headerItem.dataElementId} component={headerItem.component} placeholder={headerItem.placeholder} label={headerItem.label} value={headerItem.value} selected={headerItem.selected} />
             ))}
         </nav>
     )
