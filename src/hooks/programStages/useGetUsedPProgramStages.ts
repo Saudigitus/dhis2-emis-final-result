c854169aef3c555347b2bd10b4c13658b46a3273
CelsoDeCarvalho <celsomomade361@gmail.com>
2024-09-18 14:32:02 +0200

    fix: solve error when performance programStage do not exist

--- src/hooks/programStages/useGetUsedPProgramStages.ts
+++ src/hooks/programStages/useGetUsedPProgramStages.ts
@@ -3,7 +3,7 @@ import { getDataStoreKeys } from "../../utils/commons/dataStore/getDataStoreKeys
 
 const useGetUsedPProgramStages = () => {
     const { performance, finalResult } = getDataStoreKeys();
-    const performanceProgramStages = performance?.programStages.map((programStage: simpleProgramStage) => programStage.programStage);
+    const performanceProgramStages = performance?.programStages?.map((programStage: simpleProgramStage) => programStage?.programStage);
     return [...performanceProgramStages, finalResult?.programStage]
 }
 export default useGetUsedPProgramStages
