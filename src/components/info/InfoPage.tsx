6eeda0bfa6e6c981b5ce30e92501a89f77d1620f
CelsoDeCarvalho <celsomomade361@gmail.com>
2024-09-19 11:39:15 +0200

    enhance: changed info description if there no ou selected

--- src/components/info/InfoPage.tsx
+++ src/components/info/InfoPage.tsx
@@ -6,16 +6,15 @@ export default function InfoPage() {
     return (
         <div className={styles.containerInit}>
             <Paper elevation={1} className={styles.paperInit}>
-                <h2>SEMIS-Final Result</h2>
+                <h2>SEMIS-Staff Re-enroll</h2>
                 <span>Follow the instructions to proceed:</span>
                 <ul>
                     <li className={styles.paperOtherText}>Select the  Organization unit you want to view data</li>
-                    <li className={styles.paperOtherText}>Use global filters(Class, Grade and Academic Year)</li>
+                    <li className={styles.paperOtherText}>Use Academic Year to filter data</li>
                 </ul>
                 <span>How to perform operations:</span>
                 <ul>
-                    <li className={styles.paperOtherText}><strong>Assing final result:</strong> select the students you want to assign final result and click <strong>Assign final result</strong> button.</li>
-                    <li className={styles.paperOtherText}><strong>Bulk Promotion:</strong> select the students you want to promote and click <strong>Bulk promotion</strong> button</li>
+                    <li className={styles.paperOtherText}><strong>Bulk Promotion:</strong> select the staffs you want to re-enroll and click <strong>Perform staff re-enroll</strong> button</li>
                 </ul>
             </Paper>
         </div>
